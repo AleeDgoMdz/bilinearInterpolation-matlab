e9fb511206f2bb863cc6dc4035a06e2c96448817
AleeDgoMdz <fernando.durango@ingenieria.unam.edu>
2022-09-29 16:30:47 -0500

    --interpolacion.m   commented lines  do the same that the loop.   That was used to proof the loop with just one time   The loop generate matrix with data(x,y,precipValue) each time   Eliminate rows with NaN data and rows duplicates   Create a  object (f) to interpolate (and extrapolate)   Interpolate over a matrix   store the data in a 3D array

--- interpolacion.m
+++ interpolacion.m
@@ -2,20 +2,37 @@
 long = -ncread("datos/CC_Temps_Vars_2000.nc","Longitude");
 lat = ncread("datos/CC_Temps_Vars_2000.nc","Latitude");
 precip = ncread("datos/CC_Temps_Vars_2000.nc","PRECIP");
+[time,~] = size(precip);
+
 
 %Limpieza de datos (eliminacion de nan data y datos)
-data = [long lat precip(1,:)'];
-filData = data(sum(isnan(data),2)==0,:);
-filData = double(unique(filData(:,:),"rows","first"));
+%data = [long lat precip(1,:)'];
+%filData = data(sum(isnan(data),2)==0,:);
+%filData = double(unique(filData(:,:),"rows","first"));
 
-%Obtencion xgrid y ygrid para el meshgrid
-xgrid = -117.12512:0.018: -86.74157;
-ygrid =  14.54628 :0.018:  32.71284;
+%Obtencion xgrid y ygrid para la interpolacion
+ xgrid = -117.12512:0.018: -86.74157;
+ ygrid =  14.54628 :0.018:  32.71284;
 
-%[xq,yq] = meshgrid(xgrid,ygrid); grid data para el metodo griddata
-%interpolation = griddata(xdata,ydata,zdata,xq,yq); Opcion relativamente
-%lenta para interpolar
-
+%Generacion de la malla interpolada f
 [x,y]= ndgrid(xgrid,ygrid);
-f= scatteredInterpolant(filData(:,1:2),filData(:,3),"linear","linear");
-interData = f(x,y);
+%f= scatteredInterpolant(filData(:,1:2),filData(:,3),"linear","linear");
+%interData = f(x,y); %Interpolacion y extrapolacion a partir de la malla f
+
+
+
+%Lectura del shpfile y deliminatacion de la malla interpolada
+%mx = shaperead("shpMx/mx.shp","UseGeoCoords",true);
+
+for ntime=1:time
+    if ntime ==32
+        break
+    end
+    data = [long lat precip(ntime,:)'];
+    filData = data(sum(isnan(data),2)==0,:);
+    filData = double(unique(filData(:,:),"rows","first"));
+    f= scatteredInterpolant(filData(:,1:2),filData(:,3),"linear","linear");
+    interData = f(x,y);
+    interYear(:,:,ntime) = interData;
+    disp(strcat("fin de ",num2str(ntime)))
+end
