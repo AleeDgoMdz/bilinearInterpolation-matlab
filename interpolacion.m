394b2e38c73c5e471e4c1ffa2729e5abccd26ab8
AleeDgoMdz <fernando.durango@ingenieria.unam.edu>
2022-09-29 16:40:28 -0500

    --interpolacion.m   Base script for future changes   commented lines eleminated

--- interpolacion.m
+++ interpolacion.m
@@ -4,23 +4,11 @@ lat = ncread("datos/CC_Temps_Vars_2000.nc","Latitude");
 precip = ncread("datos/CC_Temps_Vars_2000.nc","PRECIP");
 [time,~] = size(precip);
 
-
-%Limpieza de datos (eliminacion de nan data y datos)
-%data = [long lat precip(1,:)'];
-%filData = data(sum(isnan(data),2)==0,:);
-%filData = double(unique(filData(:,:),"rows","first"));
-
-%Obtencion xgrid y ygrid para la interpolacion
+%Obtencion xgrid y ygrid (arrays) para generar la malla a interpolar
  xgrid = -117.12512:0.018: -86.74157;
  ygrid =  14.54628 :0.018:  32.71284;
-
-%Generacion de la malla interpolada f
+%Generacion de los ejes de la malla (x,y matrices)
 [x,y]= ndgrid(xgrid,ygrid);
-%f= scatteredInterpolant(filData(:,1:2),filData(:,3),"linear","linear");
-%interData = f(x,y); %Interpolacion y extrapolacion a partir de la malla f
-
-
-
 %Lectura del shpfile y deliminatacion de la malla interpolada
 %mx = shaperead("shpMx/mx.shp","UseGeoCoords",true);
 
