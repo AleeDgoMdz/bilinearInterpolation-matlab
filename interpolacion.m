8f761a9f6831fe302e950fc3291e6d04a037ecbe
AleeDgoMdz <fernando.durango@ingenieria.unam.edu>
2022-10-03 11:14:20 -0500

    --.gitignore   *.tif data agreggate --interpolacion.m   mx_2000.tif has been agreggated to the workspace to apply like mask   from intepolation matrix..

--- interpolacion.m
+++ interpolacion.m
@@ -5,12 +5,11 @@ precip = ncread("datos/CC_Temps_Vars_2000.nc","PRECIP");
 [time,~] = size(precip);
 
 %Obtencion xgrid y ygrid (arrays) para generar la malla a interpolar
- xgrid = -117.12512:0.018: -86.74157;
- ygrid =  14.54628 :0.018:  32.71284;
+ xgrid = -118.3651143520000062:0.018: -86.703114352000000;
+ ygrid =  14.5386535700000010 :0.018:  32.7186535700000007;
 %Generacion de los ejes de la malla (x,y matrices)
 [x,y]= ndgrid(xgrid,ygrid);
-%Lectura del shpfile y deliminatacion de la malla interpolada
-%mx = shaperead("shpMx/mx.shp","UseGeoCoords",true);
+
 
 for ntime=1:time
     if ntime ==32
@@ -24,3 +23,18 @@ for ntime=1:time
     interYear(:,:,ntime) = interData;
     disp(strcat("fin de ",num2str(ntime)))
 end
+meanJan = mean(interYear,3);
+
+%Lectura del raster
+[A,R] = readgeoraster("mx_2000.tif");
+
+A1 = double(A1);
+A1 = flipud(A)
+A2 = change(A1,'==',0,NaN  );
+pcolor(A2)
+shading flat,colorbar
+
+
+meanMask_01 = A2.*meanJan';
+pcolor(meanMask_01)
+shading flat,colorbar
